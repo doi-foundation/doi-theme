42a8201b73eed723ac26a7d5457ad64f6e2abbc2
Bruce Murray <github@bcgwebdesign.co.uk>
2022-09-14 10:49:56 +0100

    some number animation

--- static/js/script.js
+++ static/js/script.js
@@ -40,5 +40,60 @@ const observer = new IntersectionObserver(entries => {
   
   observerlower.observe(document.querySelector('.circle-line-lower'));
 
+$(function() {
+
+  var triggerAtY = $('#leftbignumber').offset().top - $(window).outerHeight();
+
+  $(window).scroll(function(event) {
+    // #target not yet in view
+    if (triggerAtY > $(window).scrollTop()) {
+      return;
+    }
+
+    // run your task
+    // Animate the element's value from x to y:
+    $({someValue: 1}).animate({someValue: 1000000000}, {
+      duration: 3000,
+      easing:'swing', // can be anything
+      step: function() { // called on every step
+          // Update the element's text with rounded-up value:
+          $('#leftbignumber').text(commaSeparateNumber(Math.round(this.someValue)));
+      },
+      done: function() {
+        $('#leftbignumber').text(commaSeparateNumber(1000000000) + "+");
+      }
+    });
+
+    $({someValue: 1}).animate({someValue: 292000000}, {
+      duration: 3000,
+      easing:'swing', // can be anything
+      step: function() { // called on every step
+          // Update the element's text with rounded-up value:
+          $('#rightbignumber').text(commaSeparateNumber(Math.round(this.someValue)));
+      },
+      done: function() {
+        $('#rightbignumber').text(commaSeparateNumber(292000000) + "+");
+      }
+    });
+
+
+
+    // remove this event handler
+    $(this).off(event);
+  }); 
+
+
+  
+
+  function commaSeparateNumber(val){
+  while (/(\d+)(\d{3})/.test(val.toString())){
+    val = val.toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
+  }
+  return val;
+  }
+});
+
+
+
 
   
